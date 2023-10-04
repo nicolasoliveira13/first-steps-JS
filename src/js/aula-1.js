7ddca49d3d67545a7bc60daacb5054e676d8afbe
Nicolas de Oliveira <nicolasoliveira13@gmail.com>
2023-10-04 12:46:24 -0300

    Aula 1 ajustada

---
+++ src/js/aula-1.js
@@ -0,0 +1,45 @@
+function exercicioUm() {
+  let nome = "Meu nome é Nicolas de Oliveira";
+  alert(nome);
+}
+
+function exercicioDois() {
+  let idade = "Tenho 29 anos";
+  alert(idade);
+}
+
+function exercicioTres() {
+  let nome = "Nicolas de Oliveira";
+  let idade = "29 anos";
+  alert(nome);
+  alert(idade);
+}
+
+function exercicioQuatro() {
+  let nome = "Nicolas de Oliveira";
+  let idade = 29;
+  alert(nome);
+  alert(idade + 20);
+}
+
+function exercicioCinco() {
+  let ator1 = "Gabriel Macht," + " 51 anos," + " Masculino," + " Harvey Specter.";
+  let ator2 = "\nPatrick J. Adams," + " 42 anos," + " Masculino," + " Mike Ross.";
+  let ator3 = "\nRick Hoffman," + " 53 anos," + " Masculino," + " Louis Litt.";
+  let ator4 = "\nMaghan Markle," + " 42 anos," + " Feminino," + " Rachel Zane.";
+  let ator5 = "\nSarah Rafferty," + " 50 anos," + " Feminino," + " Donna Paulsen.";
+  alert(ator1);
+  alert(ator2);
+  alert(ator3);
+  alert(ator4);
+  alert(ator5);
+}
+
+function desafio() {
+    let ator1 = "Gabriel Macht," + " 51 anos," + " Masculino," + " Harvey Specter.";
+    let ator2 = "\nPatrick J. Adams," + " 42 anos," + " Masculino," + " Mike Ross.";
+    let ator3 = "\nRick Hoffman," + " 53 anos," + " Masculino," + " Louis Litt.";
+    let ator4 = "\nMaghan Markle," + " 42 anos," + " Feminino," + " Rachel Zane.";
+    let ator5 = "\nSarah Rafferty," + " 50 anos," + " Feminino," + " Donna Paulsen.";
+    alert(ator1 + ator2 + ator3 + ator4 + ator5);
+  }
